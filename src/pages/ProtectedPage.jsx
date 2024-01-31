103fad48cb7595bb94c30dc478529990c99d15f3
Adam Gorring <adamgorring@gmail.com>
2024-01-31 18:47:24 +1000

    Added Amplify Authenticator

---
+++ src/pages/ProtectedPage.jsx
@@ -0,0 +1,32 @@
+import { Authenticator } from "@aws-amplify/ui-react";
+import { Routes, Link, useMatch, Route } from "react-router-dom";
+
+export const ProtectedPage = () => {
+  const match = useMatch({
+    path: "/protected", // Replace this with your actual path
+    end: false, // Replace with the appropriate option for your use case
+    caseSensitive: false, // Replace with the appropriate option for your use case
+  });
+
+  if (!match) {
+    throw new Error("No match found!"); // Handle the case when no match is found
+  }
+
+  return (
+    <Authenticator>
+      {({ signOut, user }) => {
+        return (
+          <div>
+            <div>
+              <button onClick={signOut}>Sign out</button>
+            </div>
+            <h3>Protected Page</h3>
+            <div>
+              <span>Hi, {user.username}</span>
+            </div>
+          </div>
+        );
+      }}
+    </Authenticator>
+  );
+};
