0312dd1092a2027fa4a436e6b507806a613e539f
Adam Gorring <adamgorring@gmail.com>
2024-02-01 16:46:55 +1000

    Added styling to bg and sidebar, added new pages with navigation

--- src/components/LeftSidebar.jsx
+++ src/components/LeftSidebar.jsx
@@ -2,64 +2,32 @@ import React from "react";
 import * as NavigationMenu from "@radix-ui/react-navigation-menu";
 import "../styles.css";
 import { useAuthenticator } from "@aws-amplify/ui-react";
+import { sidebarLinks } from "../constants";
+import { Link } from "react-router-dom";
 
 const LeftSidebar = () => {
   const { user, signOut } = useAuthenticator((context) => [context.user]);
 
   return (
-    <NavigationMenu.Root orientation="vertical" className="LeftSidebar">
-      <NavigationMenu.List>
-        <NavigationMenu.Item>
-          <NavigationMenu.Trigger className="NavigationMenuTrigger">
-            Home
-          </NavigationMenu.Trigger>
-          <NavigationMenu.Content className="NavigationMenuContent">
-            {/* Content for Item 1 */}
-          </NavigationMenu.Content>
+    <NavigationMenu.Root orientation="vertical" className="h-full ml-10 mt-10">
+      <NavigationMenu.List className="flex flex-col gap-20">
+        <NavigationMenu.Item className="flex-1">
+          <NavigationMenu.Link className="rounded font-bold text-3xl text-white">
+            a.m.gorring
+          </NavigationMenu.Link>
         </NavigationMenu.Item>
 
-        <NavigationMenu.Item>
-          <NavigationMenu.Trigger className="NavigationMenuTrigger">
-            Stocks
-          </NavigationMenu.Trigger>
-          <NavigationMenu.Content className="NavigationMenuContent">
-            {/* Content for Item 2 */}
-          </NavigationMenu.Content>
-        </NavigationMenu.Item>
+        {sidebarLinks.map((link) => (
+          <NavigationMenu.Item key={link.label} className="">
+            <Link to={link.route} className="font-semibold">
+              {link.label}
+            </Link>
+          </NavigationMenu.Item>
+        ))}
 
-        <NavigationMenu.Item>
-          <NavigationMenu.Trigger className="NavigationMenuTrigger">
-            Crypto
-          </NavigationMenu.Trigger>
-          <NavigationMenu.Content className="NavigationMenuContent">
-            {/* Content for Item 3 */}
-          </NavigationMenu.Content>
-        </NavigationMenu.Item>
-
-        <NavigationMenu.Item>
-          <NavigationMenu.Trigger className="NavigationMenuTrigger">
-            News
-          </NavigationMenu.Trigger>
-          <NavigationMenu.Content className="NavigationMenuContent">
-            {/* Content for Item 4 */}
-          </NavigationMenu.Content>
-        </NavigationMenu.Item>
-
-        <NavigationMenu.Item>
-          <NavigationMenu.Trigger className="NavigationMenuTrigger">
-            Profile
-          </NavigationMenu.Trigger>
-          <NavigationMenu.Content className="NavigationMenuContent">
-            {/* Content for Item 5 */}
-          </NavigationMenu.Content>
-        </NavigationMenu.Item>
-
-        <NavigationMenu.Item>
-          <NavigationMenu.Link
-            onSelect={signOut} // Attach the function to the onSelect prop
-            className="NavigationMenuTrigger"
-          >
-            Sign out
+        <NavigationMenu.Item className="">
+          <NavigationMenu.Link onSelect={signOut} className="font-semibold">
+            Sign Out
           </NavigationMenu.Link>
         </NavigationMenu.Item>
       </NavigationMenu.List>
---
+++ src/constants/index.js
@@ -0,0 +1,22 @@
+export const sidebarLinks = [
+  {
+    imgURL: "/assets/icons/home.svg",
+    route: "/",
+    label: "Home",
+  },
+  {
+    imgURL: "/assets/icons/wallpaper.svg",
+    route: "/stocks",
+    label: "Stocks",
+  },
+  {
+    imgURL: "/assets/icons/people.svg",
+    route: "/news",
+    label: "News",
+  },
+  {
+    imgURL: "/assets/icons/bookmark.svg",
+    route: "/saved",
+    label: "Profile",
+  },
+];
